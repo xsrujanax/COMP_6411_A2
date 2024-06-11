e5751c07ecd3b28fc428aaddb149a08d81fc6382
Srujana Guttula <142763229+xsrujanax@users.noreply.github.com>
2024-06-10 22:41:48 -0400

    Add files via upload

---
+++ COMP_6411_A2/src/menu.clj
@@ -0,0 +1,38 @@
+(ns menu
+  (:require [clojure.string :as string]
+            [db :refer [initialize-data display-customers-data display-products-data display-sales-data calculate-total-sales calculate-total-count]]))
+
+(defn display-menu []
+  (loop []
+    (println "\n*** Sales Menu ***")
+    (println "1. Display Customer Table")
+    (println "2. Display Product Table")
+    (println "3. Display Sales Table")
+    (println "4. Total Sales for Customer")
+    (println "5. Total Count for Product")
+    (println "6. Exit")
+    (print "Enter an option: ")
+    (flush)  ; Ensure that the print statement is output before blocking on read-line
+    (let [choice (read-line)]
+      (println "[DEBUG] choice entered:" choice)  ; Debug output to see what's read
+      (case choice
+        "1" (do (display-customers-data) (recur))
+        "2" (do (display-products-data) (recur))
+        "3" (do (display-sales-data) (recur))
+        "4" (do (println "Enter customer name:")
+                (let [customer-name (read-line)]
+                  (calculate-total-sales customer-name))
+                (recur))
+        "5" (do (println "Enter product description:")
+                (let [product-description (read-line)]
+                  (calculate-total-count product-description))
+                (recur))
+        "6" (do (println "Good Bye") (System/exit 0))
+        (do (println "Invalid option, please try again")
+            (recur))))))
+
+; Initialize the data
+(db/initialize-data)
+
+; Start the menu
+(display-menu)
